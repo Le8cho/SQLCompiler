ace0ed820bca92cb55b30e75673f1b90c8c23a61
Leonardo Campos <107323291+Le8cho@users.noreply.github.com>
2024-02-15 16:11:44 -0500

    Implementacion del algoritmo python en el proyecto java creacion de la clase cola aun no implementada adicion de la palabra LIKE en el diccionario

--- SQLCompiler/src/sqlpackage/SQLCompiler.java
+++ SQLCompiler/src/sqlpackage/SQLCompiler.java
@@ -1,11 +1,17 @@
 
 package sqlpackage;
 
+import java.util.ArrayList;
+
 public class SQLCompiler {
     public static void main(String[] args) {
         String sqlFilePath = "SQL_FILE.sql";
-        String sqlCode = SQLFileReader.readFile(sqlFilePath);
+        String sqlCode = SQLFileReader.readFile(sqlFilePath).toUpperCase();
         System.out.println(sqlCode);
+        
+        ArrayList<Token> tokens = Tokens.lex(sqlCode);
+        
+        System.out.println(tokens.toString());
         
     }   
 }
